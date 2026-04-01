27a62cdf15ba97ad0fd2bdaa23d665414e5f11ae
tho071206-ther <tho071206@gmail.com>
2026-04-01 10:51:49 +0700

    Khoi tao Thu muc

---
+++ QuanLyLich_HQTCSDL/Models/TaskModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyLich_HQTCSDL.Models
+{
+    internal class TaskModel
+    {
+    }
+}
